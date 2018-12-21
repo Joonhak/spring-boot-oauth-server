6420e1390d7e7264dcbd6affe9b86cba1f21fb6d
Joonhak LEE <ofnullable@gmail.com>
2018-12-21 18:41:30 +0900

    Change auth method ( access-token to jwt )
    # Save client to database
    inMemory -> implements ClientDetails, save database
    # Change access token to jwt
    jdbc access token -> jwt

--- src/test/java/io/joonhak/repository/RoleRepoTest.java
+++ src/test/java/io/joonhak/repository/RoleRepoTest.java
@@ -1,6 +1,7 @@
 package io.joonhak.repository;
 
-import io.joonhak.entity.Role;
+import io.joonhak.entity.account.Role;
+import io.joonhak.repository.account.RoleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
