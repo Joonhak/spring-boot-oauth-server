7cdaa108d5bcf585a4c657ef49259e705b5b0a4b
Joonhak <ofnullable@gmail.com>
2018-12-27 21:27:03 +0900

    Add CORS filter

--- src/main/java/io/joonhak/service/OAuthClientService.java
+++ src/main/java/io/joonhak/service/OAuthClientService.java
@@ -54,7 +54,6 @@ public class OAuthClientService implements ClientDetailsService {
 							, null
 							, null
 					);
-					
 					log.info("SAVE DEFAULT CLIENT.. {}", this.save(client));
 				}
 		);
